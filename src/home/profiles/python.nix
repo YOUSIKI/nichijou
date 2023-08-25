bcc6a1c65cd8f13a96b5652e0c21cfe6cccaa726
yousiki <you.siki@outlook.com>
2023-08-25 20:03:28 +0800

    Add PKU mirror to python

--- src/home/profiles/python.nix
+++ src/home/profiles/python.nix
@@ -29,4 +29,23 @@ in {
       then linuxPackages
       else []
     );
+
+  home.file.".config/pip/pip.conf".text = ''
+    [global]
+    index-url = https://mirrors.pku.edu.cn/pypi/simple
+  '';
+
+  home.file.".condarc".text = ''
+    changeps1: false
+    channels:
+      - defaults
+    show_channel_urls: true
+    default_channels:
+      - https://mirrors.pku.edu.cn/anaconda/pkgs/main
+      - https://mirrors.pku.edu.cn/anaconda/pkgs/r
+    custom_channels:
+      conda-forge: https://mirrors.pku.edu.cn/anaconda/cloud
+      pytorch: https://mirrors.pku.edu.cn/anaconda/cloud
+      bioconda: https://mirrors.pku.edu.cn/anaconda/cloud
+  '';
 }
