f15234c029cf1a7aa0fcd253c60dbe596d3be764
yousiki <you.siki@outlook.com>
2023-10-17 00:10:26 +0800

    Update home sessionVariables EDITOR

--- src/homeProfiles/base.nix
+++ src/homeProfiles/base.nix
@@ -4,7 +4,16 @@
   pkgs,
   ...
 }:
-with builtins // lib; {
+with builtins // lib; let
+  editor =
+    if config.programs.neovim.enable
+    then "nvim"
+    else if config.programs.helix.enable
+    then "hx"
+    else if config.programs.vim.enable
+    then "vim"
+    else "nano";
+in {
   nixpkgs = {
     inherit (globals.nixpkgs) overlays;
   };
@@ -13,6 +22,11 @@ with builtins // lib; {
     if hasSuffix "-darwin" pkgs.system
     then "/Users/${config.home.username}"
     else "/home/${config.home.username}";
+
+  home.sessionVariables = {
+    EDITOR = mkDefault editor;
+    VISUAL = mkDefault editor;
+  };
 
   home.stateVersion = "23.05";
 }
