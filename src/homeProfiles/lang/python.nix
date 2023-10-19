b0b95519e3cd2527bcc17d429deff8eaa7cc5d54
yousiki <you.siki@outlook.com>
2023-10-19 22:18:00 +0800

    Update python

--- src/homeProfiles/lang/python.nix
+++ src/homeProfiles/lang/python.nix
@@ -5,7 +5,7 @@
   ...
 }:
 with builtins // lib; let
-  installationPath = "~/.miniconda";
+  minicondaPath = "~/.miniconda";
 in {
   home.packages =
     (with pkgs; [
@@ -21,34 +21,34 @@ in {
     ]));
 
   programs.bash.initExtra = ''
-    # Add conda to PATH
-    export PATH=${installationPath}/bin:$PATH
-    # Paths for gcc if compiling some C sources with pip
-    export NIX_CFLAGS_COMPILE="-I${installationPath}/include"
-    export NIX_CFLAGS_LINK="-L${installationPath}lib"
-    # Some other required environment variables
-    export FONTCONFIG_FILE=/etc/fonts/fonts.conf
-    export QTCOMPOSE=${pkgs.xorg.libX11}/share/X11/locale
-    export LIBARCHIVE=${pkgs.libarchive.lib}/lib/libarchive.so
-    # Allows `conda activate` to work properly if already installed
-    if [ -d ${installationPath}/etc/profile.d ]; then
-      source ${installationPath}/etc/profile.d/conda.sh
+    if [ -d ${minicondaPath} ]; then
+      # Add conda to PATH
+      export PATH=${minicondaPath}/bin:$PATH
+      # Paths for gcc if compiling some C sources with pip
+      export NIX_CFLAGS_COMPILE="-I${minicondaPath}/include"
+      export NIX_CFLAGS_LINK="-L${minicondaPath}lib"
+      # Some other required environment variables
+      export FONTCONFIG_FILE=/etc/fonts/fonts.conf
+      export QTCOMPOSE=${pkgs.xorg.libX11}/share/X11/locale
+      export LIBARCHIVE=${pkgs.libarchive.lib}/lib/libarchive.so
+      # Allows `conda activate` to work properly
+      source ${minicondaPath}/etc/profile.d/conda.sh
     fi
   '';
 
   programs.zsh.initExtra = ''
-    # Add conda to PATH
-    export PATH=${installationPath}/bin:$PATH
-    # Paths for gcc if compiling some C sources with pip
-    export NIX_CFLAGS_COMPILE="-I${installationPath}/include"
-    export NIX_CFLAGS_LINK="-L${installationPath}lib"
-    # Some other required environment variables
-    export FONTCONFIG_FILE=/etc/fonts/fonts.conf
-    export QTCOMPOSE=${pkgs.xorg.libX11}/share/X11/locale
-    export LIBARCHIVE=${pkgs.libarchive.lib}/lib/libarchive.so
-    # Allows `conda activate` to work properly if already installed
-    if [ -d ${installationPath}/etc/profile.d ]; then
-      source ${installationPath}/etc/profile.d/conda.sh
+    if [ -d ${minicondaPath} ]; then
+      # Add conda to PATH
+      export PATH=${minicondaPath}/bin:$PATH
+      # Paths for gcc if compiling some C sources with pip
+      export NIX_CFLAGS_COMPILE="-I${minicondaPath}/include"
+      export NIX_CFLAGS_LINK="-L${minicondaPath}lib"
+      # Some other required environment variables
+      export FONTCONFIG_FILE=/etc/fonts/fonts.conf
+      export QTCOMPOSE=${pkgs.xorg.libX11}/share/X11/locale
+      export LIBARCHIVE=${pkgs.libarchive.lib}/lib/libarchive.so
+      # Allows `conda activate` to work properly
+      source ${minicondaPath}/etc/profile.d/conda.sh
     fi
   '';
 
