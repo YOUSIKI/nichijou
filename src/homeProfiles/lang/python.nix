f04cd6b466d00ffe6694ed1ac8b63d99e8d3158a
YouSiki <You.Siki@outlook.com>
2024-02-18 16:40:44 +0800

    Add Python configuration files

--- src/homeProfiles/lang/python.nix
+++ src/homeProfiles/lang/python.nix
@@ -17,4 +17,35 @@ in {
     ruff-lsp
     yapf
   ];
+
+  home.file.".condarc".text = ''
+    channels:
+      - defaults
+    changeps1: false
+    show_channel_urls: true
+    auto_activate_base: false
+    default_channels:
+      - https://mirrors.pku.edu.cn/anaconda/pkgs/main
+      - https://mirrors.pku.edu.cn/anaconda/pkgs/r
+    custom_channels:
+      Paddle: https://mirrors.pku.edu.cn/anaconda/cloud
+      bioconda: https://mirrors.pku.edu.cn/anaconda/cloud
+      conda-forge: https://mirrors.pku.edu.cn/anaconda/cloud
+      intel: https://mirrors.pku.edu.cn/anaconda/cloud
+      numba: https://mirrors.pku.edu.cn/anaconda/cloud
+      pytorch3d: https://mirrors.pku.edu.cn/anaconda/cloud
+      pytorch: https://mirrors.pku.edu.cn/anaconda/cloud
+      rapidsai: https://mirrors.pku.edu.cn/anaconda/cloud
+  '';
+
+  home.file.".config/pip/pip.conf".text = ''
+    [global]
+    index-url = https://mirrors.pku.edu.cn/pypi/web/simple
+  '';
+
+  programs.zsh.initExtra = ''
+    if [[ -x "$(command -v conda)" ]]; then
+      eval "$(conda "shell.$(basename "$SHELL")" hook)"
+    fi
+  '';
 }
