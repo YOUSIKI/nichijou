e730bbc5111e92921a43b22a8955f8ae017fc06e
YouSiki <You.Siki@outlook.com>
2024-02-18 23:11:49 +0800

    Update system packages in packages.nix

--- src/commonProfiles/packages.nix
+++ src/commonProfiles/packages.nix
@@ -6,30 +6,34 @@
   ...
 }:
 with builtins // lib; {
-  environment.systemPackages = with pkgs; [
-    alejandra
-    curl
-    duf
-    eza
-    fd
-    fzf
-    gdu
-    gh
-    git
-    helix
-    home-manager
-    htop
-    jq
-    man
-    neofetch
-    nodejs
-    nvfetcher
-    ripgrep
-    rsync
-    vim
-    wget
-    zellij
-  ];
+  environment.systemPackages =
+    (with pkgs; [
+      alejandra
+      curl
+      duf
+      eza
+      fd
+      fzf
+      gdu
+      gh
+      git
+      helix
+      home-manager
+      htop
+      jq
+      man
+      neofetch
+      nodejs
+      nvfetcher
+      ripgrep
+      rsync
+      vim
+      wget
+      zellij
+    ])
+    ++ (optionals pkgs.stdenv.isDarwin (with globals.outputs.packages.${pkgs.system}; [
+      lporg
+    ]));
 
   programs.fish.enable = true;
   programs.tmux.enable = true;
