5701aa745d1b38e0ddde7bdd81f2e640cbfa1094
yousiki <you.siki@outlook.com>
2024-03-17 06:46:20 +0000

    Update sakamoto configuration

--- src/configurations/darwin/sakamoto/default.nix
+++ src/configurations/darwin/sakamoto/default.nix
@@ -1,36 +1,35 @@
-# Configuration for sakamoto, which is an intel MacbookPro.
-{globals, ...}: let
-  darwinModules = with globals.outputs; [
-    # Host-specific modules
-    ./_applications.nix
-    ./_configuration.nix
+# Configuration for sakamoto.
+{globals, ...}:
+globals.inputs.darwin.lib.darwinSystem {
+  system = "x86_64-darwin";
+  specialArgs = {inherit globals;};
+  modules =
+    [
+      ./_applications.nix
+      ./_configuration.nix
 
-    # Host specific profiles
-    commonProfiles.nix
-    commonProfiles.packages
-    darwinProfiles.homebrew
-
-    # Home-manager module
-    globals.inputs.home-manager.darwinModules.home-manager
-    {
-      home-manager = {
-        useGlobalPkgs = true;
-        useUserPackages = true;
-        extraSpecialArgs = {inherit globals;};
-        users.yousiki = {
-          imports = with homeProfiles; [
-            base
-            lang.complete
-            shell
-            ssh
-          ];
+      globals.inputs.home-manager.darwinModules.home-manager
+      {
+        home-manager = {
+          useGlobalPkgs = true;
+          useUserPackages = true;
+          extraSpecialArgs = {inherit globals;};
+          users.yousiki = {
+            imports = with globals.outputs.homeProfiles; [
+              base
+              lang.complete
+              shell
+              ssh
+            ];
+          };
         };
-      };
-    }
-  ];
-in
-  globals.inputs.darwin.lib.darwinSystem {
-    system = "x86_64-darwin";
-    specialArgs = {inherit globals;};
-    modules = darwinModules;
-  }
+      }
+    ]
+    ++ (with globals.outputs.commonProfiles; [
+      nix
+      packages
+    ])
+    ++ (with globals.outputs.darwinProfiles; [
+      homebrew
+    ]);
+}
