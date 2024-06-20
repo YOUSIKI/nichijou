5564d62721a697dfd0fa65608192280c7daa8c4d
yousiki <you.siki@outlook.com>
2024-06-20 20:47:01 +0800

    Switch to xanmod kernel

--- cells/nixos/hosts/hakase/configuration.nix
+++ cells/nixos/hosts/hakase/configuration.nix
@@ -10,7 +10,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
   networking.hostName = "hakase";
 }
