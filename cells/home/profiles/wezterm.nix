f29c3d573973649f4688dd34cc658edcb58f5d17
YouSiki <You.Siki@outlook.com>
2024-06-21 15:20:27 +0800

    Revise wezterm config

--- cells/home/profiles/wezterm.nix
+++ cells/home/profiles/wezterm.nix
@@ -8,30 +8,29 @@
   ...
 }: let
   l = builtins // lib;
+
+  weztermConfig = ''
+    local wezterm = require 'wezterm'
+
+    local config = {}
+
+    if wezterm.config_builder then
+      config = wezterm.config_builder()
+    end
+
+    config.color_scheme = 'Catppuccin Mocha'
+
+    config.font = wezterm.font_with_fallback {
+      'CaskaydiaCove Nerd Font',
+      'Jetbrains Nerd Font',
+      'Fira Code',
+      'Source Code Pro',
+    }
+
+    config.font_size = 13.0
+
+    return config
+  '';
 in {
-  programs.wezterm = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    extraConfig = ''
-      local wezterm = require 'wezterm'
-
-      local config = {}
-
-      if wezterm.config_builder then
-        config = wezterm.config_builder()
-      end
-
-      config.font = wezterm.font_with_fallback {
-        'CaskaydiaCove Nerd Font',
-        'Jetbrains Nerd Font',
-        'Fira Code',
-        'Source Code Pro',
-      }
-
-      config.font_size = 13.0
-
-      return config
-    '';
-  };
+  home.file.".config/wezterm/wezterm.lua".text = weztermConfig;
 }
