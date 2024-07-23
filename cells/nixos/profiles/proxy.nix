f1653acb3322eb7948f35992037393cf7bff4767
yousiki <you.siki@outlook.com>
2024-07-23 20:55:13 +0800

    Remove clash-meta, switch to mihomo.

--- cells/nixos/profiles/proxy.nix
+++ cells/nixos/profiles/proxy.nix
@@ -6,18 +6,15 @@
   config,
   ...
 }: {
-  imports = [
-    inputs.cells.nixos.nixosModules.clash-meta
-  ];
-
   networking.proxy.default = "http://127.0.0.1:7890";
-  networking.proxy.noProxy = "127.0.0.1,localhost,yousiki.top,siki.moe,edu.cn";
+  networking.proxy.noProxy = "127.0.0.1,localhost,siki.moe,yousiki.top,ybh1998.space,edu.cn";
 
   services.cloudflare-warp.enable = true;
 
-  services.clash-meta.enable = true;
-  services.clash-meta.configPath = config.age.secrets.clash-config.path;
-  services.clash-meta.openFirewall = true;
+  services.mihomo = {
+    enable = true;
+    configFile = config.age.secrets.clash-config.path;
+  };
 
   age.secrets.clash-config.file = "${inputs.self}/secrets/clash-config.yaml.age";
 }
