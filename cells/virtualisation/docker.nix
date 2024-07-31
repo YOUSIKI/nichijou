94f8f6da1e0a6c8cc9b0d5e25981e6aca8495d8e
yousiki <you.siki@outlook.com>
2024-07-31 21:23:37 +0800

    Refactor docker.nix file

--- cells/virtualisation/docker.nix
+++ cells/virtualisation/docker.nix
@@ -9,10 +9,6 @@
       enable = true;
       enableOnBoot = true;
       autoPrune.enable = true;
-      daemon.settings = {
-        ipv6 = true;
-        registry-mirrors = ["https://docker.siki.moe"];
-      };
     };
   };
   environment.systemPackages = with pkgs; [
