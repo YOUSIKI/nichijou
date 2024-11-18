b1fd26c4ae3f4368a8f54b1d39496cc140b454c4
YouSiki <you.siki@outlook.com>
2024-11-18 20:27:36 +0800

    add GitHub Actions workflow for checks and update wezterm configuration style

--- modules/home/programs/terminal/wezterm/wezterm.lua
+++ modules/home/programs/terminal/wezterm/wezterm.lua
@@ -1,17 +1,17 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-    config = wezterm.config_builder()
-end 
+	config = wezterm.config_builder()
+end
 
 local function scheme_for_appearance(appearance)
-    if appearance:find "Dark" then
-        return "Catppuccin Mocha"
-    else
-        return "Catppuccin Frappe"
-    end
+	if appearance:find("Dark") then
+		return "Catppuccin Mocha"
+	else
+		return "Catppuccin Frappe"
+	end
 end
 
 local appearance = wezterm.gui.get_appearance()
@@ -19,13 +19,13 @@ local appearance = wezterm.gui.get_appearance()
 -- config.color_scheme = scheme_for_appearance(appearance)
 config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font_with_fallback {
-    'CaskaydiaCove Nerd Font',
-    'Cascadia Code PL',
-    'Cascadia Code',
-    'Fira Code',
-    'Source Code Pro',
-}
+config.font = wezterm.font_with_fallback({
+	"CaskaydiaCove Nerd Font",
+	"Cascadia Code PL",
+	"Cascadia Code",
+	"Fira Code",
+	"Source Code Pro",
+})
 
 config.font_size = 13.0
 
