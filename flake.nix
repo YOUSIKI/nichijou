685de1aeddb3d917cf308cdca0786b2286668237
YouSiki <you.siki@outlook.com>
2024-11-19 00:04:33 +0800

    feat: add additional trusted substituters and public keys in flake.nix

--- flake.nix
+++ flake.nix
@@ -94,17 +94,27 @@
     substituters = [
       "https://cache.garnix.io"
       "https://cache.nixos.org"
+      "https://colmena.cachix.org"
+      "https://cuda-maintainers.cachix.org"
+      "https://hyprland.cachix.org"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://nichijou.cachix.org"
       "https://nix-community.cachix.org"
+      "https://nixpkgs-wayland.cachix.org"
       "https://numtide.cachix.org"
     ];
     trusted-substituters = substituters;
     trusted-public-keys = [
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
+      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+      "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      "nichijou.cachix.org-1:rbaTU9nLgVW9BK/HSV41vsag6A7/A/caBpcX+cR/6Ps="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
     ];
   };
