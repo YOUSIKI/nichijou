2a14b9f1a9bd8522c1a447dfa3f661d2f13e2740
YouSiki <you.siki@outlook.com>
2024-11-18 23:25:38 +0800

    add cache.garnix.io as a trusted substituter and public key in flake.nix

--- flake.nix
+++ flake.nix
@@ -92,6 +92,7 @@
 
   nixConfig = rec {
     substituters = [
+      "https://cache.garnix.io"
       "https://cache.nixos.org"
       "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
@@ -101,6 +102,7 @@
     ];
     trusted-substituters = substituters;
     trusted-public-keys = [
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
