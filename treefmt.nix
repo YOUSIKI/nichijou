a03be7519923cfbe03831a6bea127e27858edfc1
YouSiki <you.siki@outlook.com>
2024-11-18 23:55:35 +0800

    feat: add support for multiple formatting and checking tools in treefmt.nix

--- treefmt.nix
+++ treefmt.nix
@@ -1,9 +1,38 @@
-# treefmt.nix
-{...}: {
+_: {
   # Used to find the project root
   projectRootFile = "flake.nix";
-  # Enable alejandra for nix
-  programs.alejandra.enable = true;
-  # Enable stylua for lua
-  programs.stylua.enable = true;
+
+  # Enabled programs
+  programs = {
+    # Enable alejandra for nix formatting
+    alejandra.enable = true;
+    # Enable deadnix for nix checking
+    deadnix.enable = true;
+    # Enable keep-sorted for general sorting
+    keep-sorted.enable = true;
+    # Enable prettier for json, yaml, toml formatting
+    prettier.enable = true;
+    # Enable stylua for lua formatting
+    stylua.enable = true;
+    # Enable shellcheck for shell checking
+    shellcheck.enable = true;
+    # Enable shfmt for shell formatting
+    shfmt.enable = true;
+    # Enable statix for nix checking
+    statix.enable = true;
+    # Enable typos for spell checking
+    typos.enable = true;
+  };
+
+  # Settings
+  settings.formatter = {
+    prettier.includes = [
+      "*.json"
+      "*.yaml"
+      "*.toml"
+    ];
+    typos.excludes = [
+      "static/*"
+    ];
+  };
 }
