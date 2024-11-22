decbed66cd70e0230d84712e2086c0790fa7fa34
YouSiki <you.siki@outlook.com>
2024-11-22 13:13:36 +0800

    nix: Update nano configuration with hostname and system preferences

--- systems/aarch64-darwin/nano/default.nix
+++ systems/aarch64-darwin/nano/default.nix
@@ -1,4 +1,22 @@
 {pkgs, ...}: {
+  # Set the hostname and computer name
+  networking.hostName = "nano";
+  networking.computerName = "YouSiki's Nano";
+
+  # Add ability to used TouchID for sudo authentication
+  security.pam.enableSudoTouchIdAuth = true;
+
+  # System configurations
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = false;
+  system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+  system.defaults.dock.show-recents = false;
+  system.defaults.dock.tilesize = 48;
+  system.defaults.finder.QuitMenuItem = true;
+  system.defaults.finder.ShowPathbar = true;
+  system.defaults.finder.ShowStatusBar = true;
+  system.defaults.trackpad.Clicking = true;
+  system.defaults.trackpad.TrackpadThreeFingerDrag = true;
+
   environment.systemPackages = with pkgs; [
     alejandra
   ];
