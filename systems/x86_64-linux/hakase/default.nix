283f5c0437b3b1d5eb16a0602d239cb3fb5da69b
YouSiki <you.siki@outlook.com>
2024-12-12 21:46:47 +0800

    Add KDE and SDDM to hakase

--- systems/x86_64-linux/hakase/default.nix
+++ systems/x86_64-linux/hakase/default.nix
@@ -54,6 +54,13 @@
     };
   };
 
+  services = {
+    xserver.enable = true;
+    displayManager.sddm.enable = true;
+    displayManager.sddm.wayland.enable = true;
+    desktopManager.plasma6.enable = true;
+  };
+
   nichijou = {
     system = {
       filesys = {
