8e907e1ae019de4b61ac4e90c6957ea81347af84
YouSiki <you.siki@outlook.com>
2025-01-25 13:42:38 +0800

    Enable Tailscale in the default.nix configuration

--- systems/x86_64-linux/hakase/default.nix
+++ systems/x86_64-linux/hakase/default.nix
@@ -99,6 +99,7 @@
     displayManager.sddm.enable = true;
     displayManager.sddm.wayland.enable = true;
     desktopManager.plasma6.enable = true;
+    tailscale.enable = true;
   };
 
   nichijou = {
