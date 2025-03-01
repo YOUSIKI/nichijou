a6ad540c60e11400872b9b881aa04f077144b954
YouSiki <you.siki@outlook.com>
2025-03-02 00:26:52 +0800

    Add extra configuration options for kitty terminal tab behavior

--- modules/home/programs/graphical/kitty/default.nix
+++ modules/home/programs/graphical/kitty/default.nix
@@ -29,6 +29,13 @@ in {
         enableZshIntegration = true;
       };
       themeFile = "Catppuccin-Mocha";
+      extraConfig = ''
+        tab_bar_min_tabs            1
+        tab_bar_edge                bottom
+        tab_bar_style               powerline
+        tab_powerline_style         slanted
+        tab_title_template          {title}{" :{}:".format(num_windows) if num_windows > 1 else ""}
+      '';
     };
   };
 }
